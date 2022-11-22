86deabe97fd2ce6917c6b85ae1ab78fe41bd9f40
Nate Stodd <nathan.stoddard@gmail.com>
2022-11-22 10:26:59 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+// Index, GET /pokemon
+
+// Show, GET /pokemon/:id
+
+// New, GET /pokemon/new
+
+// Edit, GET /pokemon/:id/edit
+
+// Create, POST /pokemon
+
+// Update, PUT /pokemon/:id
+
+// Destroy, DELETE /pokemon/:id
+

01b785db0f195af0a21c699c13eeaf8a594f1ecd
Nate Stodd <nathan.stoddard@gmail.com>
2022-12-06 19:45:37 -0500

    mvp?

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ require("dotenv").config()
 // GLOBAL VARIABLES
 //************************* */
 const PORT = process.env.PORT || 3210
-const POKEMON = require("./models/pokemon.js")
+let POKEMON = require("./models/pokemon.js")
 
 //************************* */
 // EXPRESS OBJECT
@@ -36,7 +36,7 @@ app.get('/', (req, res) => {
     console.log(POKEMON)
     res.redirect("/pokemon")
 })
-
+ 
 // Index, GET /pokemon
 app.get('/pokemon', (req, res) => {
     res.render("index.ejs", {
@@ -44,31 +44,68 @@ app.get('/pokemon', (req, res) => {
     })
 })
 
-// Show, GET /pokemon/:id
-app.get('/pokemon/:id', (req, res) => {
-    res.render("show.ejs", {
-        data: Pokemon[req.params.id]
-    })
-})
-
 // New, GET /pokemon/new
 app.get('/pokemon/new', (req, res) => {
+    res.render("new.ejs")
+})
 
+// Create, POST /pokemon
+app.post('/pokemon', (req, res) => {
+    console.log(req.body.name)
+    const newPokemon = {
+        name: req.body.name,
+        img: req.body.img,
+        type: req.body.types.split(","),
+        stats: {
+            hp: req.body.HP,
+            attack: req.body.AP,
+            defense: req.body.DP
+        }
+    }
+    POKEMON.push(newPokemon)
+    res.redirect("/pokemon")
 })
 
 // Edit, GET /pokemon/:id/edit
 app.get('/pokemon/:id/edit', (req, res) => {
-
+    res.render("edit.ejs", {
+        pokemon : POKEMON[req.params.id],
+        index: req.params.id
+    })
 })
 
-// Create, POST /pokemon
-app.post('/pokemon')
 
 // Update, PUT /pokemon/:id
-app.put('/pokemon/:id')
+app.put('/pokemon/:id', (req, res) => {
+    const updatedPokemon = {
+        name: req.body.name,
+        img: req.body.img,
+        type: req.body.types.split(","),
+        stats: {
+            hp: req.body.HP,
+            attack: req.body.AP,
+            defense: req.body.DP
+        }
+    }
+    POKEMON[req.params.id] = updatedPokemon
+    res.redirect(`/pokemon/${req.params.id}`)
+})
 
 // Destroy, DELETE /pokemon/:id
-app.delete('/pokemon/:id')
+app.delete('/pokemon/:id', (req, res) => {
+    POKEMON.splice(req.params.id, 1)
+    res.redirect("/pokemon")
+})
+
+// Show, GET /pokemon/:id
+app.get('/pokemon/:id', (req, res) => {
+    console.log(req.params.id)
+    console.log(POKEMON[req.params.id])
+    res.render("show.ejs", {
+        data: POKEMON[req.params.id],
+        index: req.params.id
+    })
+})
 
 //************************* */
 // SERVER LISTENER
